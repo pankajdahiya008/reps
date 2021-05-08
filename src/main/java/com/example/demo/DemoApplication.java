2ac390ff22628aef734860a7dbcffa6dac3663e6
Pankaj Dahiya <pankaj.id.008@gmail.com>
2021-05-08 19:18:11 +0530

    java 16 spring

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,13 +1,13 @@
-package com.sapient.reps;
+package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class RepsWorksApplication {
+public class DemoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(RepsWorksApplication.class, args);
+		SpringApplication.run(DemoApplication.class, args);
 	}
 
 }
--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,10 +1,10 @@
-package com.sapient.reps;
+package com.example.demo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class RepsWorksApplicationTests {
+class DemoApplicationTests {
 
 	@Test
 	void contextLoads() {
